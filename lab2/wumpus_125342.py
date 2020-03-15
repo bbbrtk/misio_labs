f8a85df452b372896d3d122fa91b6457cf9e8ccd
bbbrtk <sobkowiak.bart@gmail.com>
2020-03-15 23:31:13 +0100

    wumpus v1.0

--- lab2/wumpus_125342.py
+++ lab2/wumpus_125342.py
@@ -12,41 +12,114 @@ PREC = '.2f'
 # (B) calc prob for each combination with pit on queried cell - sum up 
 # prob for a pit in queried cell is B/A
 
-class Query:
+class Wumpus:
     def __init__(self, world, p):
         self.p = p
         self.world = world
+        self.breezes = []
         self.fringe = []
         self.combinations = []
         self.probabilities = []
+        self.dict_of_fringe_probs = {}
         self.sum_of_probabilities = 1  
      
+
     def find_fields_to_calculate(self):
         x = len(self.world[1])
         y = len(self.world)
         for i in range(y):
             for j in range(x): 
                 if self.world[i][j] == '-1.0': self.fringe.append((i,j))
+                if self.world[i][j] == '2.00': self.breezes.append((i,j))
         
-        print("fringe: \n", self.fringe)
+        # print("fringe: \n", self.fringe)
+
+
+    def check_if_combination_is_sufficient(self, combination):
+        ''' combination should cover all B (==2.0) fields on board '''
+        breezes_copy = self.breezes.copy()
+        # print(breezes_copy)
+        for each in combination:
+            x = each[0]
+            y = each[1]
+            if (x+1,y) in breezes_copy: breezes_copy.remove((x+1,y))
+            if (x-1,y) in breezes_copy: breezes_copy.remove((x-1,y))
+            if (x,y+1) in breezes_copy: breezes_copy.remove((x,y+1))
+            if (x,y-1) in breezes_copy: breezes_copy.remove((x,y-1))
+
+            if len(breezes_copy) == 0: 
+                return True
+
+        return False
+
 
     def generate_all_combinations(self):
-        for L in range(1, len(self.fringe)+1):
-            for subset in itertools.combinations(self.fringe, L):
-                self.combinations.append(subset)
+        ''' create list of combinations of all possible pits on board
+        for each element in self.fringe create list of indexes to 
+        particular combinations which contain this element '''
+        for each in self.fringe:
+            self.dict_of_fringe_probs[each] = []
 
-        print("combinations: \n", self.combinations)
+        j = 0
+        for i in range(1, len(self.fringe)+1):
+            for subset in itertools.combinations(self.fringe, i):
+                if self.check_if_combination_is_sufficient(subset):
+                    self.combinations.append(subset)
+                    # print("sufficient combination: ", subset)
+                    for each in subset:
+                        self.dict_of_fringe_probs[each].append(j)
+                    j += 1
+
+        # print("combinations len: ", len(self.combinations))
+        # print("combinations: \n", self.combinations)
+        # print("dict_of_fringe_probs: \n", self.dict_of_fringe_probs)
+
 
     def probability_for_combinations(self):
         max_num_of_pits = len(self.fringe)
-        for each in self.combinations:
-            num_of_pits = len(each)
-            num_of_empties = max_num_of_pits - num_of_pits
-            probability = round(p**num_of_pits * (1-p)**num_of_empties,4)
-            self.probabilities.append(probability)
-        self.sum_of_probabilities = sum(self.probabilities)
 
+        for combination in self.combinations:
+            num_of_pits = len(combination)
+            num_of_empties = max_num_of_pits - num_of_pits
+            probability = p**num_of_pits * (1-p)**num_of_empties
+            self.probabilities.append(probability) # change rounding
+
+        self.sum_of_probabilities = sum(self.probabilities)
         print("probabilities: \n", self.probabilities)
+
+
+    def sum_up_probabilities(self, query):
+        indexes = self.dict_of_fringe_probs[query]
+        p_sum = 0
+        for index in indexes:
+            p_sum += self.probabilities[index]
+        return p_sum
+
+
+    def calculate_probabilities(self):
+        sum_of_all = sum(self.probabilities)
+        # print("sum_of_all: ", round(sum_of_all,4))
+        for each in self.fringe:
+            sum_of_query = self.sum_up_probabilities(each)
+            q = round(sum_of_query / sum_of_all, 2)
+            # print("field: ", each, " \t sum_of_query: ", round(sum_of_query, 4), "\t probability: ", q)
+            self.world[each[0]][each[1]] = q
+
+
+    def change_breeze_fileds_to_zero(self):
+        x = len(self.world[1])
+        y = len(self.world)
+        for i in range(y):
+            for j in range(x): 
+                if self.world[i][j] == '1.0':
+                    self.world[i][j] = format(1, PREC)
+                if self.world[i][j] == '2.00':
+                    self.world[i][j] = format(0, PREC)
+                elif self.world[i][j] == '-1.O':
+                    print(" !!! ERROR: -1 FOUND !!! ")
+
+    def get_world(self):
+        return self.world
 
 
 
@@ -55,7 +128,7 @@ def print_result(lines, output_file):
     print(result, file=output_file, flush=True)
 
 
-def prewumpus(world, p):
+def pre_wumpus(world, p):
     '''return matrix with 0.20 when unknown (can't calculate), 
     0 when known (breeze B or visited O) and -1 for fields to calculate'''    
     x = len(world[1])
@@ -70,7 +143,7 @@ def prewumpus(world, p):
     for i in range(1,y+1):
         for j in range(1,x+1): 
             if world[i][j] == 'B':
-                ret[i][j] = format(1, PREC)
+                ret[i][j] = format(2, PREC)
                 if ret[i-1][j]==p: ret[i-1][j] = -1.0
                 if ret[i+1][j]==p: ret[i+1][j] = -1.0
                 if ret[i][j-1]==p: ret[i][j-1] = -1.0
@@ -83,13 +156,17 @@ def prewumpus(world, p):
     return ret[1:-1, 1:-1]
 
 
-def wumpus(world,p):
-    query = Query(world, p)
-    query.find_fields_to_calculate()
-    query.generate_all_combinations()
-    query.probability_for_combinations()
+def wumpus_wumpus(world,p):
+    wumpus = Wumpus(world, p)
 
-    print("board:")
+    wumpus.find_fields_to_calculate()
+    wumpus.generate_all_combinations()
+    wumpus.probability_for_combinations()
+    wumpus.calculate_probabilities()
+    wumpus.change_breeze_fileds_to_zero()
+
+    world = wumpus.get_world()
+    # print("board:")
 
     return world
 
@@ -105,9 +182,9 @@ if __name__ == "__main__":
     for _ in range(instances_num):
         world, p = load_world(input_file)
         # wumups
-        world = prewumpus(world, p)
-        lines = wumpus(world, p)
+        world = pre_wumpus(world, p)
+        lines = wumpus_wumpus(world, p)
         # out
         print_result(lines, output_file)
-        print("--------------") # del
+
 
