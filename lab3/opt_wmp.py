57dbcf104264a269a9326e4ff3ee358f97c1f92f
bbbrtk <sobkowiak.bart@gmail.com>
2020-04-19 22:13:56 +0200

    speed v1

--- lab3/opt_wmp.py
+++ lab3/opt_wmp.py
@@ -18,10 +18,11 @@ PERCENTAGE_OF_MOST_PROBABLE_POINTS = 1
 PRINT_ALL = False
 
 
-class MyWumpus(object):
-    def __init__(self, map: np.ndarray, p: float, pj: float, pn: float, exit_loc: tuple = None):
+class OptWmp(object):
+    def __init__(self, map: np.ndarray, p: float, pj: float, pn: float, exit_loc: tuple = None, max_moves=None):
+        assert isinstance(map, np.ndarray)
         self.map = map
-        self.board = None
+        self.board = map.astype('float64')
         self.h, self.w = map.shape
         self.p = p
         self.p_rest = (1-p)/4
@@ -49,17 +50,20 @@ class MyWumpus(object):
                 self.exit_border_h = ('UP', up_ey)
 
         self.exit_loc = list(exit_loc)
+        self.position = list(exit_loc)
+        self.moves = np.inf
         self.finished = True
+        self.sensory_output = None
+        if max_moves is None:
+            max_moves = np.inf
+        self.max_moves = max_moves
 
-        self.signal = Field.EMPTY
+        self.signal = self.position
+        self.random_moves = []
+        self.path = {Action.LEFT : 0, Action.DOWN : 0, Action.RIGHT : 0,  Action.UP : 0}
         self.last_move = Action.RIGHT
         self.my_path = []
         self.first = True
-        self.empties = np.argwhere(map == Field.EMPTY)
-        self.caves = np.argwhere(map == Field.CAVE)
-        self.emp_probs = 0
-        self.cav_probs = 0
-        self.more_empties = True
 
     def __str__(self):
         return super().__str__()
@@ -71,6 +75,33 @@ class MyWumpus(object):
     def sense(self, sensory_input: bool):
         self.signal = sensory_input
 
+    def first_analyze(self):
+        all_fields = self.h * self.w - 1
+        field_prob = 1/all_fields
+        
+        for i in range(self.h):
+            for j in range(self.w):
+                self.board[i,j] = float(field_prob)
+
+                if self.map[i,j] == Field.CAVE:     self.board[i,j] *= self.pj
+                elif self.map[i,j] == Field.EMPTY:  self.board[i,j] *= self.pn
+                elif self.map[i,j] == Field.EXIT:   self.board[i,j] = 0
+
+
+    def cave_analyze(self):
+        for i in range(self.h):
+            for j in range(self.w):
+                if self.map[i,j] == Field.CAVE:     self.board[i,j] *= self.pj
+                elif self.map[i,j] == Field.EMPTY:  self.board[i,j] *= self.pn
+                elif self.map[i,j] == Field.EXIT:   self.board[i,j] = 0
+
+
+    def empty_analyze(self):
+        for i in range(self.h):
+            for j in range(self.w):
+                if self.map[i,j] == Field.CAVE:     self.board[i,j] = 0
+                # elif self.map[i,j] == Field.EMPTY:  self.board[i,j] *= self.pn
+                elif self.map[i,j] == Field.EXIT:   self.board[i,j] = 0
 
     # check if this move is not in the opposite direction
     def block_counter_move(self, move):
@@ -87,27 +118,31 @@ class MyWumpus(object):
     # sum up and choose the most often occured direction
     def select_move_direction(self):
         # if PRINT_ALL: print('- move -')
-        the_path = {Action.LEFT : 0, Action.DOWN : 0, Action.RIGHT : 0,  Action.UP : 0}
-        greatest_fields = np.argwhere(self.board >= np.amax(self.board))
+        if (self.signal == Field.EXIT): self.finished = True
+        else:
+            greatest_fields = np.argwhere(
+                self.board >= np.amax(self.board) * PERCENTAGE_OF_MOST_PROBABLE_POINTS
+                )
+            # greatest_fields = np.argwhere(self.board > 0) 
+            for field in greatest_fields:
+                field_path = self.find_path_to_exit(tuple(field))
+                # update
+                # field_prob = self.board[field[0], field[1]] 
+                for key in self.path.keys(): self.path[key] += field_path[key] # * field_prob
+                # print(field, " - > ", field_path)
+            
+            # if PRINT_ALL: print(self.path)
+            # sort dict
+            m = {k: v for k, v in sorted(self.path.items(), key=lambda item: item[1])}
 
-        for field in greatest_fields:
-            field_path = self.find_path_to_exit(tuple(field))
-            # field_path = self.path_to_exit_dict[tuple(field)]
-            # update
-            # field_prob = self.board[field[0], field[1]] 
-            for key in the_path.keys(): the_path[key] += field_path[key] # * field_prob
-            # print(field, " - > ", field_path)
-        
-        # if PRINT_ALL: print(the_path)
-        # sort dict
-        m = {k: v for k, v in sorted(the_path.items(), key=lambda item: item[1])}
+            # select best move, get next one if it's counter move
+            move = list(m.keys())[-1]
+            if self.block_counter_move(move):
+                move = list(m.keys())[-2]
 
-        # select best move, get next one if it's counter move
-        move = list(m.keys())[-1]
-        if self.block_counter_move(move):
-            move = list(m.keys())[-2]
-
-        return move
+            # zeroing dict
+            self.path = {key: 0 for key in self.path}
+            return move
 
     # find manhatan distance for PARTICULAR field
     def find_path_to_exit(self, field):
@@ -130,7 +165,9 @@ class MyWumpus(object):
             path[Action.LEFT] = x_distance
         elif x_distance < 0:
             path[Action.RIGHT] = abs(x_distance)
-
+        else: 
+            # do nothnig
+            pass
         #
         # distance when goes through UP/DOWN borders
         if self.exit_border_h[0] == 'DOWN': # distance from upper border
@@ -146,6 +183,9 @@ class MyWumpus(object):
             path[Action.UP] = y_distance
         elif y_distance < 0:
             path[Action.DOWN] = abs(y_distance)
+        else: 
+            # do nothnig
+            pass
 
         return path
 
@@ -207,55 +247,55 @@ class MyWumpus(object):
 
     # multiply field and its neighbours by p or p_rest
     def recalulate_field(self, i, j, boards_copy):
-        ij_p_rest = boards_copy[i,j]*self.p_rest
         # destination
         self.board[i,j]             += boards_copy[i,j]*self.p
         # down
         if i+1 >= self.h:
-            self.board[0,j]         += ij_p_rest
+            self.board[0,j]         += boards_copy[i,j]*self.p_rest
         else: 
-            self.board[i+1,j]       += ij_p_rest
+            self.board[i+1,j]       += boards_copy[i,j]*self.p_rest
         # up
         if i-1 < 0:
-            self.board[self.h-1,j]  += ij_p_rest
+            self.board[self.h-1,j]  += boards_copy[i,j]*self.p_rest
         else: 
-            self.board[i-1,j]       += ij_p_rest
+            self.board[i-1,j]       += boards_copy[i,j]*self.p_rest
         # right
         if j+1 >= self.w:
-            self.board[i,0]         += ij_p_rest
+            self.board[i,0]         += boards_copy[i,j]*self.p_rest
         else: 
-            self.board[i,j+1]       += ij_p_rest
+            self.board[i,j+1]       += boards_copy[i,j]*self.p_rest
         # left
         if j-1 < 0:
-            self.board[i, self.w-1] += ij_p_rest
+            self.board[i, self.w-1] += boards_copy[i,j]*self.p_rest
         else: 
-            self.board[i,j-1]       += ij_p_rest   
+            self.board[i,j-1]       += boards_copy[i,j]*self.p_rest      
 
-    # ?
-    def recalulate_field_2(self, i, j):
+    # multiply field and its neighbours by 1/len(possible)
+    def recalulate_field_probs(self, i, j, prob):
         # destination
-        self.board[i,j]             *= self.p
+        self.board[i,j]             += prob*self.p
         # down
         if i+1 >= self.h:
-            self.board[0,j]         *= self.p_rest
+            self.board[0,j]         += prob*self.p_rest
         else: 
-            self.board[i+1,j]       *= self.p_rest
+            self.board[i+1,j]       += prob*self.p_rest
         # up
         if i-1 < 0:
-            self.board[self.h-1,j]  *= self.p_rest
+            self.board[self.h-1,j]  += prob*self.p_rest
         else: 
-            self.board[i-1,j]       *= self.p_rest
+            self.board[i-1,j]       += prob*self.p_rest
         # right
         if j+1 >= self.w:
-            self.board[i,0]         *= self.p_rest
+            self.board[i,0]         += prob*self.p_rest
         else: 
-            self.board[i,j+1]       *= self.p_rest
+            self.board[i,j+1]       += prob*self.p_rest
         # left
         if j-1 < 0:
-            self.board[i, self.w-1] *= self.p_rest
+            self.board[i, self.w-1] += prob*self.p_rest
         else: 
-            self.board[i,j-1]       *= self.p_rest   
-     
+            self.board[i,j-1]       += prob*self.p_rest    
+
+        
     # multiply WHOLE BOARD by p or p_rest
     def recalculate_probabilities(self):
         boards_copy = self.board.copy()
@@ -265,29 +305,11 @@ class MyWumpus(object):
             for j in range(self.w):
                 self.recalulate_field(i,j,boards_copy)
 
-    # !!!
-    def recalulate(self):
-        if self.signal == Field.EMPTY:
-            if self.more_empties:
-                self.board += self.emp_probs
-                for f in self.caves:
-                    self.board[f[0],f[1]] -= self.emp_probs
-            else:
-                for f in self.empties:
-                    self.board[f[0],f[1]] += self.emp_probs
-        
-        elif self.signal == Field.CAVE:
-            if not self.more_empties:
-                self.board += self.cav_probs
-                self.board *= self.pj
-                for f in self.empties:
-                    self.board[f[0],f[1]] = ((self.board[f[0],f[1]]/self.pj) - self.cav_probs) * self.pn
-
-            else:
-                self.board *= self.pn
-                for f in self.caves:
-                    self.board[f[0],f[1]] = (self.board[f[0],f[1]]/self.pn + self.cav_probs) * self.pj
-
+    # update only possible ends of paths
+    def recalculate_only_possible_fields(self, possible):
+        # self.board = np.zeros((self.h, self.w))
+        for field in possible:
+            self.recalulate_field_probs(field[0], field[1], 1/len(possible))
 
     # find all fields which are the ENDPOINTS
     # of already visited path on board
@@ -342,43 +364,67 @@ class MyWumpus(object):
             p_sum += sum(row)
 
         if p_sum > 0:
-            self.board = self.board / p_sum
-            # for i in range(self.h):
-            #     for j in range(self.w):
-            #         self.board[i,j] /= p_sum
-            
-            # for row in self.board:
-            #     row = [x/p_sum for x in row]
+            for i in range(self.h):
+                for j in range(self.w):
+                    self.board[i,j] /= p_sum
+
+    def random_move(self):
+        if PRINT_ALL: print('- random move -')
+        if len(self.random_moves) % 2 == 0: 
+            move = Action.UP
+        else:
+            if random.random() > 0.5: move = Action.LEFT
+            else: move = Action.RIGHT
+
+        self.random_moves.append(move)
+        return move
 
     # agent.move()
     def move(self):
-
+        """
+        1. recalulate_field() po wyborze ruchu w zadanym kierunku
+        2. recalculate_only_possible_fields:
+            CAVE - 1/possible pomnożyć całość przez pj i pn
+            EMPTY- 1/possible tylko dla EMPTY
+        """
         # if PRINT_ALL: print('- normal move -')
         # if PRINT_ALL: print(Field(self.signal))
         # self.signal = int(input())
 
         # append last move to path
         if self.first:
-            self.first = False
             self.board = np.zeros((self.h, self.w))
-            self.emp_probs = 1/len(self.empties)
-            self.cav_probs = 1/len(self.caves)
-            if self.emp_probs > self.cav_probs: 
-                self.more_empties = False
+            self.first = False
+        else:
+            pass
+
+        # find all possible sub-path endpoints        
+        possible = np.argwhere(self.map == self.signal) 
 
         # recalucalate probs
-        self.recalulate()
+        # self.recalculate_only_possible_fields(possible)
+
+        if self.signal == Field.EMPTY:
+            probs = 1/len(possible)
+            for f in possible:
+                self.board[f[0],f[1]] += probs
+        
+        elif self.signal == Field.CAVE:
+            probs = 1/len(possible)
+            for f in possible:
+                self.board[f[0],f[1]] += probs
+                self.board[f[0],f[1]] *= self.pj
+
+            others = np.argwhere(self.map == Field.EMPTY) 
+            for f in others:
+                self.board[f[0],f[1]] *= self.pn
 
         self.normalize()
-        self.board[self.exit_loc[0],self.exit_loc[1]] = 0
         # if PRINT_ALL: print_result(self.board)  
 
         # select direction
         self.last_move = self.select_move_direction()
         # print(self.last_move, file=OUTPUT_FILE, flush=True)
-
-        # self.recalculate_probabilities()
-        # self.normalize()
 
         # move probabilities in selected direction
         self.update_2(self.last_move)
@@ -425,7 +471,7 @@ def load_all_from_file():
     worlds = load_input_file(input_file)
     for i in worlds:
         world, p, pj, pn = i
-        wumpus = MyWumpus(world, p, pj, pn)
+        wumpus = OptWmp(world, p, pj, pn)
         wumpus.reset()
         wumpus.init_move()
         # do stuff
@@ -433,8 +479,7 @@ def load_all_from_file():
 
 if __name__ == "__main__":
     # load_all_from_file()
-    run_agent(MyWumpus)
+    run_agent(OptWmp)
 
 
         
-
--- lab3/test.py
+++ lab3/test.py
@@ -3,6 +3,7 @@
 from misio.lost_wumpus.testing import test_locally
 from misio.lost_wumpus.agents import RandomAgent, SnakeAgent
 from lost_wumpus import MyWumpus
+from opt_wmp import OptWmp
 import numpy as np
 import time
 
@@ -13,12 +14,12 @@ n = 10
 # test_locally("tests/2016.in", RandomAgent, n=n)
 
 t1 = time.time()
-# test_locally("tests/2015.in", SnakeAgent, n=n)
-test_locally("tests/2020.in", SnakeAgent, n=n)
+test_locally("tests/2015.in", SnakeAgent, n=n)
+# test_locally("tests/2015.in", OptWmp, n=n)
 print(time.time() - t1)
 
 t1 = time.time()
 # test_locally("tests/2015.in", MyWumpus, n=n)
-test_locally("tests/2020.in", MyWumpus, n=n)
+test_locally("tests/2015.in", MyWumpus, n=n)
 
 print(time.time() - t1)
